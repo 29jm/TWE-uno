aa105ab784789611804e4953b0ae5f8566c6b285
AC-G <67235625+AC-G@users.noreply.github.com>
2020-06-23 14:28:33 +0200

    Added a rules page, improved style

--- templates/footer.php
+++ templates/footer.php
@@ -4,6 +4,7 @@
 			// Si l'utilisateur est connecté, on affiche un lien de déconnexion
 			if (valider("connected", "SESSION")) {
 				echo "<a href=\"index.php?action=Logout\">Se déconnecter</a>";
+				echo "<a href=\"rules.html\">Consulter les règles du jeu</a>";
 			}
 		?>
 	</div>
